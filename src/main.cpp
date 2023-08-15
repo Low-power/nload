0b16f150764a4f78b17baeb68ea3cdb6dc8ff846
WHR <msl0000023508@gmail.com>
2023-08-15 21:20:02 +0800

    Support switching to human readable decimal units display

--- src/main.cpp
+++ src/main.cpp
@@ -97,8 +97,10 @@ int main(int argc, char *argv[])
     SettingStore::get("MultipleDevices").pushFilter(new SettingFilterMap(valueMapping));
     valueMapping.clear();
 
-    valueMapping[toString(Statistics::humanReadableBit)] = "Human Readable (Bit)";
-    valueMapping[toString(Statistics::humanReadableByte)] = "Human Readable (Byte)";
+    valueMapping[toString(Statistics::humanReadableBit)] = "Human Readable (Binary Bit)";
+    valueMapping[toString(Statistics::humanReadableByte)] = "Human Readable (Binary Byte)";
+    valueMapping[toString(Statistics::humanReadableSiBit)] = "Human Readable (Decimal Bit)";
+    valueMapping[toString(Statistics::humanReadableSiByte)] = "Human Readable (Decimal Byte)";
     valueMapping[toString(Statistics::bit)] = "Bit";
     valueMapping[toString(Statistics::byte)] = "Byte";
     valueMapping[toString(Statistics::kibiBit)] = "KiBit";
